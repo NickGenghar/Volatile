1068b7afee7d74d26a7fa494b982b671f2071bea
Nick Genghar <www.genghar15@gmail.com>
2020-05-13 08:48:17 +0800

    Scaffold for event based triggers.

---
+++ events/message.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+/**
+ * @param {Object} msg Discord.js Message object.
+ * @param {Array<String>} args Collections of arguments.
+ */
+module.exports = (msg, args) => {
+
+}
